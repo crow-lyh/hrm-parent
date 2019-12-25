e0354b83f1d6fed4530fce8bf3033e7f8d07c6e6
corw <369240707@qq.com>
2019-12-25 19:36:05 +0800

    项目搭建

---
+++ hrm-supports-parent/hrm-eureka-server/src/main/java/cn/itsource/hrm/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package cn.itsource.hrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description TODO
+ * @Author ZL
+ * @Date 2019/12/22 9:17
+ * @Version v1.0
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
