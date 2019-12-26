665b64f05bd555954471166c56b9541aeb5ef6e8
corw <369240707@qq.com>
2019-12-26 11:42:12 +0800

    dd

---
+++ hrm-course-parent/hrm-course-interface/src/main/java/cn/itsource/hrm/query/CourseTypeQuery.java
@@ -0,0 +1,17 @@
+package cn.itsource.hrm.query;
+
+
+
+import cn.itsource.basic.query.BaseQuery;
+
+/**
+ * <p>
+ * 课程目录 查询参数对象
+ * </p>
+ *
+ * @author crow
+ * @since 2019-12-26
+ */
+public class CourseTypeQuery extends BaseQuery {
+
+}
